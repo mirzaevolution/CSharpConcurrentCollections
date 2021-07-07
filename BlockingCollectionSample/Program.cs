863e49219752d4ad2404a562ea6d20c8c7ef305a
Mirza Ghulam Rasyid <mirzaevolution@gmail.com>
2021-07-07 12:02:24 +0700

    Add project files.

---
+++ BlockingCollectionSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlockingCollectionSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
